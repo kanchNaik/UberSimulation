b6db6209a2118fd7233470418300558b6d9b7bcb
naikn <kanchannaik55@gmail.com>
2024-12-04 13:14:15 -0800

    react routing in place

--- Frontend/uberapp/src/App.js
+++ Frontend/uberapp/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Header from "./components/Common/Header/CustomerHeader/Header";
-import RideForm from "./components/Common/Ride/RideForm";
-import Map from "./components/Common/Map/Map";
 import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+
+//All components import
+import Home from "./components/Home/Home";
 
 function App() {
   return (
@@ -10,9 +12,10 @@ function App() {
       <GlobalStyles />
       <div className="App">
         <div className="main-content">
-          <Header />
-          <RideForm />
-          <Map />
+        <Routes> 
+          <Route path="/" element={<Navigate to="/uberhome" replace />} />
+          <Route path="/uberhome" element={<Home />} />
+        </Routes>
         </div>
       </div>
     </>
---
+++ Frontend/uberapp/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../Common/Header/CustomerHeader/Header";
+import RideForm from "../Common/Ride/RideForm";
+import Map from "../Common/Map/Map";
+
+function Home() {
+  return (
+    <>
+    <Header />
+    <RideForm />
+    <Map />
+    </>
+  )
+}
+
+export default Home
