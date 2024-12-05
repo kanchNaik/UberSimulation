20f45b411b46e5187715b658b0b3db0bf71ddabe
Himani Shah <himanii.shah83@gmail.com>
2024-12-04 19:36:40 -0800

    driver main page fix.

--- Frontend/uberapp/src/components/Common/HeroSection/HeroSection.js
+++ Frontend/uberapp/src/components/Common/HeroSection/HeroSection.js
@@ -8,6 +8,9 @@ const HeroSection = () => {
             <h1>Drive and Earn on Your Schedule</h1>
             <p>Be your own boss and get paid weekly.</p>
             <a href="/driver/signup" target="_blank" className="cta-button">Sign Up to Drive</a>
+            <p> </p>
+            <p> Already have an account?</p>
+            <a href="/login" target="_blank" className="cta-button">Login</a>
         </section>
     );
 };
