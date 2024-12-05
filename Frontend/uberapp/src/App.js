763ea97561ff135bbdb8982a57e27a9ee910ae93
Chelsea Jaculina <chelseajaculina@gmail.com>
2024-12-04 21:16:49 -0800

    add customer trips list routing

--- Frontend/uberapp/src/App.js
+++ Frontend/uberapp/src/App.js
@@ -15,6 +15,8 @@ import PaymentList from "./components/Payments/PaymentList";
 import AddPaymentMethod from "./components/Payments/AddPaymentMethod";
 import CustomerProfile from "./components/UserProfile/CustomerProfile";
 import DriverProfile from "./components/UserProfile/DriverProfile";
+import TripsList from "./components/Rider/Trips/TripsList";
+
 
 window.store = store;
 
@@ -29,13 +31,14 @@ function App() {
           <Route path="/customer/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/customer/signup" element={<CustomerSignup />} />
-          <Route path="/customer/manageaccount/:id" element={<CustomerProfile />} />
-          <Route path="/customer/payments" element={<PaymentList />} />
-          <Route path="/customer/payments/add" element={<AddPaymentMethod />} />
-
-          <Route path="/driver/signup" element={<DriverSignup />} />
           <Route path="/driver" element={<Driver/>} />
           <Route path="/driver/home" element={<DriverHome />} />
+          <Route path="/customer/manageaccount/:id" element={<CustomerProfile />} />
+          <Route path="/customer/trips" element={<TripsList />} />
+
+          <Route path="/driver/signup" element={<DriverSignup />} />
+          <Route path="/customer/payments" element={<PaymentList />} />
+          <Route path="/customer/payments/add" element={<AddPaymentMethod />} />
           <Route path="/driver/manageaccount/:id" element={<DriverProfile />} />
           <Route path="*" element={<h1>404 Not Found</h1>}/>
         </Routes>
