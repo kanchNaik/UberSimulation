db8f1ff8d8081de489d64756cbc705e09e9d44dc
Himani Shah <himanii.shah83@gmail.com>
2024-12-07 16:57:10 -0800

    driver ride accept/decline.

--- Frontend/uberapp/src/components/DriverDashboard/RideRequest.js
+++ Frontend/uberapp/src/components/DriverDashboard/RideRequest.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import "./RideRequest.css";
 
-const RideRequest = ({ riderName, pickupLocation, dropoffLocation, distance, time, onAccept, onDecline }) => {
+Modal.setAppElement("#root");
+
+const RideRequest = ({
+  riderName,
+  pickupLocation,
+  dropoffLocation,
+  distance,
+  time,
+  onAccept,
+  onDecline,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [modalAction, setModalAction] = useState(null);
+
+  const openModal = (message, action) => {
+    console.log("Opening modal...");
+    setModalMessage(message);
+    setModalAction(() => action);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalAction(null);
+  };
+
+  const handleConfirm = () => {
+    if (modalAction) modalAction();
+    closeModal();
+  };
+
   return (
     <div className="ride-request-container">
       <div className="ride-details">
@@ -13,13 +45,39 @@ const RideRequest = ({ riderName, pickupLocation, dropoffLocation, distance, tim
         <p><strong>Estimated Time:</strong> {time}</p>
       </div>
       <div className="action-buttons">
-        <button className="accept-button" onClick={onAccept}>
+        <button
+          className="accept-button"
+          onClick={() => openModal("Are you sure you want to accept this ride?", onAccept)}
+        >
           Accept Ride
         </button>
-        <button className="decline-button" onClick={onDecline}>
+        <button
+          className="decline-button"
+          onClick={() => openModal("Are you sure you want to decline this ride?", onDecline)}
+        >
           Decline Ride
         </button>
       </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <div className="modal-content">
+          <h3>Confirmation</h3>
+          <p>{modalMessage}</p>
+          <div className="modal-buttons">
+            <button className="modal-confirm-button" onClick={handleConfirm}>
+              Yes
+            </button>
+            <button className="modal-cancel-button" onClick={closeModal}>
+              No
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
--- Frontend/uberapp/src/components/Home/DriverDashboard.js
+++ Frontend/uberapp/src/components/Home/DriverDashboard.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Header from "../Common/Header/DriverHeader/Header";
 import Footer from "../Common/Footer/Footer";
 import Dashboard from "../DriverDashboard/Dashboard";
-import RideRequest from "../DriverDashboard/RideRequest";
 function DriverDashboard() {
   return (
     <>
     <Header />
     <Dashboard />
-    <RideRequest />
     <Footer />
     </>
   )
